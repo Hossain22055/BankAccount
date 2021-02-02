c1b5ed1729d14c7f54897226b0a490e77fc1de70
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-02-02 13:28:13 -0500

    First Draft

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+        private double balance = 100;
+        public void Deposit(double amount)
+        {
+            balance = balance + amount;
+        }
+
+        public double GetBlance()
+        {
+            return balance; 
+        }
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Deposit your money");
+
+            var amountTODeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountTODeposit);
+
+            Console.WriteLine($"Your blance is {account.GetBlance()}");
+
+            
+
+
+
+
+          
+
+
+
+
+
+
+
+        }
+    }
+}
